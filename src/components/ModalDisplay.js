27699a4c867c20a21a784788bf39bddcee5155e4
jeremygit <jheritage@gmail.com>
2021-10-21 20:16:30 +1100

    add loaded modal

--- src/components/ModalDisplay.js
+++ src/components/ModalDisplay.js
@@ -1,5 +1,7 @@
+import React, { useState, useEffect } from 'react';
 import { useModal } from '../contexts/ModalContext';
 import { ModalComponents } from '../constants/ModalConstants';
+import { Drawer } from '@material-ui/core';
 
 export default function ModalDisplay() {
   const modal = useModal();
@@ -9,4 +11,24 @@ export default function ModalDisplay() {
   }
 
   return (null);  
+}
+
+export function ModalLoadedDisplay({children, loaded}) {
+  if (loaded) {
+    return children();
+  } else {
+    return null;
+  }
+}
+
+export function ModalContainer({ children, onClose}) {
+  return (
+    <Drawer
+      anchor="bottom"
+      open={true}
+      onClose={onClose}
+    >
+      { children }
+    </Drawer>
+  );
 }
--- src/components/modals/BLEResponseDataModal.js
+++ src/components/modals/BLEResponseDataModal.js
@@ -1,6 +1,7 @@
-import { Drawer, Container, Box } from '@material-ui/core';
+import { Container, Box } from '@material-ui/core';
 import { useBLE, BLETransferState } from '../../contexts/BLEContext';
 import { useModal } from '../../contexts/ModalContext';
+import { ModalContainer, ModalLoadedDisplay } from '../ModalDisplay';
 
 export default function BLEResponseDataModal() {
   
@@ -8,25 +9,22 @@ export default function BLEResponseDataModal() {
   const modal = useModal();
 
   return (
-    <Drawer
-      anchor="bottom"
-      open={true}
-      onClose={() => {
-        modal.hideModal();
-        ble.clearResponseData();
-      }}
-    >
-      <Container>
-        {
-          ble.transferState === BLETransferState.Idle
-          && ble.responseData
-          ? <Box>
-              <Box>User ID: {ble.responseData.userVisits[0]}</Box>
-              <Box>Marker ID: {ble.responseData.markerId}</Box>
-            </Box>
-          : <Box>Loading...</Box>
-        }
-      </Container>
-    </Drawer>
+    <ModalLoadedDisplay loaded={ble.transferState === BLETransferState.Idle}>
+      { () => (
+          <ModalContainer
+            onClose={() => {
+              modal.hideModal();
+              ble.clearResponseData();
+            }}
+          >
+            <Container>
+              <Box>
+                <Box>User ID: {ble.responseData.userVisits[0]}</Box>
+                <Box>Marker ID: {ble.responseData.markerId}</Box>
+              </Box>
+            </Container>
+          </ModalContainer>
+      ) }
+    </ModalLoadedDisplay>
   );
 }
